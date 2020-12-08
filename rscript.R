27a832fc3d0a2c84e2cb94817fbb8bc682b81392
Asbjorn Aarekol <aaa040@uit.no>
2020-12-08 14:46:04 +0100

    her morten

--- rscript.R
+++ rscript.R
@@ -8,16 +8,17 @@ library(rvest)
 #url <- "https://en.wikipedia.org/wiki/List_of_countries_by_firearm-related_death_rate"
 url <- "https://en.wikipedia.org/wiki/Estimated_number_of_civilian_guns_per_capita_by_country"
 
+# Read the happines data from the UN from the CSV and put it in a dataframe
+# Using the data from 2017 as the wikipedia data is also from 2017
+df2017 <- read_csv('2017.csv')
+
+
 # Find the table from wikipedia and return a dataframe
 table <- url %>%
   read_html() %>%
   html_nodes(xpath='//table[1]') %>% 
   html_table(fill = TRUE) %>% 
   data.frame()
-
-# Read the happines data from the UN from the CSV and put it in a dataframe
-# Using the data from 2017 as the wikipedia data is also from 2017
-df2017 <- read_csv('2017.csv')
 
 
 # Rename the variables in the wikipedia table df and merge it with the happines data
@@ -27,7 +28,9 @@ table <- table %>%
   rename("Firearms_estimate" = Estimate.of.firearms.in.civilian.possession) %>% 
   rename("Reg_firearm" = Registered.firearms) %>% 
   rename("Unreg_firearm" = Unregistered.firearms) %>% 
-  select(Country, Firearms100, Firearms_estimate, Reg_firearm, Unreg_firearm) %>% 
+  rename("Population" = Population.2017) %>% 
+  mutate(Population = as.numeric(sub(",", "", Population, value = TRUE))) %>% 
+  select(Country, Firearms100, Firearms_estimate, Reg_firearm, Unreg_firearm, Population) %>% 
   merge(., df2017)
 
 
@@ -42,11 +45,7 @@ ggplot(table, aes(reorder(Country, Firearms100), Firearms100, fill = Country)) +
 
 # Plot the firearms100 vs happines
 ggplot(table, aes(Happiness.Score, Firearms100, color = Country)) +
-  geom_jitter(aes(), show.legend = FALSE) +
-  theme(strip.text.y = element_text(angle = 0),
-        axis.text.x = element_text(angle = 45, hjust = 1, size = 6),
-        axis.title.x = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.title.y = element_blank())
+  geom_jitter(aes(), show.legend = FALSE)
+
 
  
